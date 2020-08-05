f876cd81db0d82afd4899ca2881c94ff985f3c34
Tyler Huffman <ty.huff24@gmail.com>
2020-08-05 13:08:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,16 @@
+require 'pry'
 class Song
+  self.attr_accessor :name, :artist_name #I wrotre this line
   attr_accessor :name, :artist_name
   @@all = []
+
+  def name
+    @name
+  end
+# I added the methods above and below this comment;
+  def name=(name)
+    @name = name
+  end
 
   def self.all
     @@all
@@ -8,6 +18,60 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    self.new.save
+    @@all.last
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    #binding.pry
+    song.name = name
+  end
+
+  def self.create_by_name(name)
+    self.create
+    @name = name
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if @@all.include?(name)
+      @@all.find {|song| song.name == name}
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song1, song2| song1 <=> song2}
+  end
+
+  def self.new_from_filename(file)
+    file.split("-", 4)
+    name = file[2]
+    artist_name = file[1]
+
+    Song.new(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+  end
+
+  def self.create_from_filename
+    file.split("-", 4)
+    name = file[2]
+    artist_name = file[1]
+
+    Song.create(name)
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
