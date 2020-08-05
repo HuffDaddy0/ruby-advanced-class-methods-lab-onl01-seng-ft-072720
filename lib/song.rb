bcf272fd581b0be3953badda355b2a63040afca9
Tyler Huffman <ty.huff24@gmail.com>
2020-08-05 16:25:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name != nil
+    if self.find_by_name(name) != nil
       self.find_by_name
     else
       self.create_by_name(name)
